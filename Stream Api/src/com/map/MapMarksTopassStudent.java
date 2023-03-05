12e44eade273df336b5aec2a1a95c191536dba34
KSrivastawa <kt.ketansri@gmail.com>
2023-03-05 20:30:37 +0530

    Map and Filter Example

--- Stream Api/src/com/map/MapMarksTopassStudent.java
+++ Stream Api/src/com/map/MapMarksTopassStudent.java
@@ -1,5 +1,6 @@
 package com.map;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,21 +42,40 @@ public class MapMarksTopassStudent {
 		
 		List<MapMarksTopassStudent> list = students.stream()
 		.filter(s-> s.marks<passMarks)
-		.map(m-> new MapMarksTopassStudent(m.roll, m.name, m.marks+increaseMarks))
+		.map(m-> new MapMarksTopassStudent(m.roll, m.name, m.marks=increaseMarks))
 		.collect(Collectors.toList());
 		
 		list.forEach(System.out::println);
-		System.out.println("==================student details=================");
+		System.out.println("==================student details1=================");
 		
 		List<MapMarksTopassStudent> studentList = students.stream().map(m-> m
 				.marks < passMarks
-				? new MapMarksTopassStudent(m.roll, m.name, m.marks+increaseMarks)
+				? new MapMarksTopassStudent(m.roll, m.name, m.marks=increaseMarks)
 						: new MapMarksTopassStudent(m.roll, m.name, m.marks))
 		.collect(Collectors.toList());
 
 		studentList.forEach(System.out::println);
 		
-
+		System.out.println("===================student details2=================");
+		
+		List<MapMarksTopassStudent> passedStudents = passTheStudents(students);
+		passedStudents.forEach(System.out::println);
+	}
+	
+	public static List<MapMarksTopassStudent> passTheStudents(List<MapMarksTopassStudent> students){
+		List<MapMarksTopassStudent> list = new ArrayList<>();
+		
+		for(MapMarksTopassStudent ms: students) {
+			if(ms.marks<passMarks) {
+				MapMarksTopassStudent 
+				topassStudent = new MapMarksTopassStudent
+				(ms.roll,ms.name ,ms.marks=increaseMarks); 
+				list.add(topassStudent);
+			}
+			list.add(new MapMarksTopassStudent(ms.roll,ms.name, ms.marks));
+		}
+		
+		return list;
 		
 	}
 
